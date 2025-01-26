5457d6f06614c94479a22079e7af162c882d26f1
Uglus <ugluss@gmail.com>
2025-01-26 22:40:35 +0200

    Lecture26/Created Programmer

---
+++ src/Lecture_26/src/Employees/Programmer.java
@@ -0,0 +1,35 @@
+package Lecture_26.src.Employees;
+
+import Lecture_26.src.Task;
+import Lecture_26.src.TaskProgressCallback;
+
+public class Programmer extends Employee {
+    protected Programmer(TaskProgressCallback callback, String name) {
+        super(callback, name, Task.Status.READY_TO_DO);
+    }
+
+
+    @Override
+    protected Task getTaskWhenDone(Task task) {
+        return new Task(
+                task.getId(),
+                Task.Status.READY_FOR_TESTING,
+                task.getDescription(),
+                task.getDesignLink(),
+                task.getTestCase(),
+                getBuildLinkForTask(task.getId())
+        );
+    }
+
+
+    @Override
+    protected String getDetails(Task task) {
+        return " with task id" + task.getId() +
+                " and buildLink" + task.getBuildLink() +
+                "\nand testcase" + task.getTestCase();
+    }
+
+    private String getBuildLinkForTask(int taskId) {
+        return "https://link.com/buildLink_with_id_" + taskId;
+    }
+}
