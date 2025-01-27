db1efdd73118d1c25b8b0b96bc453df49aebc4a9
Uglus <ugluss@gmail.com>
2025-01-27 23:40:39 +0200

    Lecture27 finished

--- src/Lecture_27/Lecture27.java
+++ src/Lecture_27/Lecture27.java
@@ -2,5 +2,28 @@ package Lecture_27;
 
 import Base.Lecture;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lecture27 extends Lecture {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        print(list.isEmpty());
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        list.add("fourth");
+        for(String element : list)
+            print(element);
+
+        list.remove("first");
+        for(int i = 0;i<list.size();i++){
+            print("i " + i + " item " + list.get(i));
+        }
+
+        print(list.contains("first"));
+
+    }
+
 }
